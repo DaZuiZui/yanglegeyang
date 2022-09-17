69fcbd5d757b01566e61beb53083e7ac1f0df023
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-09-17 22:23:21 +0800

    Add files via upload

---
+++ src/main/java/com/dazuizui/yang/YangApplication.java
@@ -0,0 +1,13 @@
+package com.dazuizui.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangApplication.class, args);
+    }
+
+}
